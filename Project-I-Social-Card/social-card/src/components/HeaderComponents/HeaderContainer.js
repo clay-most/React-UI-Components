d8f29a83e60cfa001957d6d815492161d2557ccf
clay-most <46730958+clay-most@users.noreply.github.com>
2019-05-29 16:45:29 -0600

    build: header container & import to app

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+import ImageThumbnail from "./ImageThumbnail";
+
+const HeaderContainer = () => {
+  return (
+    <div id="header">
+      <ImageThumbnail />
+      <div id="header-content">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderContainer;
