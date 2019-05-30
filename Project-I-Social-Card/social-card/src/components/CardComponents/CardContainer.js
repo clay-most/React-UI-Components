a3852354798db491a21f54a494cf4d7ad28efb1b
clay-most <46730958+clay-most@users.noreply.github.com>
2019-05-29 20:03:29 -0600

    build: style

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -6,10 +6,11 @@ import CardContent from "./CardContent";
 
 const CardContainer = () => {
   return (
+    <div id="card">
     <a href="https://www.reactjs.org">
       <CardBanner />
       <CardContent />
-    </a>
+    </a></div>
   );
 };
 
