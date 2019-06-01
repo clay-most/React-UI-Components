a6c960ff03a588fd365bbb85c498a7b5b3c441ee
clay-most <46730958+clay-most@users.noreply.github.com>
2019-05-31 18:30:02 -0600

    build components

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import ActionButton from "./components/ButtonComponents/ActionButton";
+import NumberButton from "./components/ButtonComponents/NumberButton";
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="wrapper">
+      <CalculatorDisplay text="0" />
+      <div className="btns">
+        <div className="number">
+          <ActionButton buttonStyle="long" text="clear" />
+          <NumberButton buttonStyle="default" text="7" />
+          <NumberButton buttonStyle="default" text="8" />
+          <NumberButton buttonStyle="default" text="9" />
+          <NumberButton buttonStyle="default" text="4" />
+          <NumberButton buttonStyle="default" text="5" />
+          <NumberButton buttonStyle="default" text="6" />
+          <NumberButton buttonStyle="default" text="1" />
+          <NumberButton buttonStyle="default" text="2" />
+          <NumberButton buttonStyle="default" text="3" />
+          <ActionButton buttonStyle="long" text="0" />
+
+          <div className="operator">
+            <NumberButton buttonStyle="red" text="÷" />
+            <NumberButton buttonStyle="red" text="×" />
+            <NumberButton buttonStyle="red" text="-" />
+            <NumberButton buttonStyle="red" text="+" />
+            <NumberButton buttonStyle="red" text="=" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
